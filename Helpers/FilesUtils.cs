f30eca7faaebb3896c18f7d58e245c1bdb267c21
iMuhammadMustafa <29302054+iMuhammadMustafa@users.noreply.github.com>
2023-11-07 17:34:27 -0600

    Add File Generation Utils
    - Create Directory if it doesn't exist
    - Download File
    - Save HttpResponse to file
    - Unzip Files
    - Rename File and Directory
    
    - Rename Solution Folder and Solution File

---
+++ Helpers/FilesUtils.cs
@@ -0,0 +1,64 @@
+﻿using Spectre.Console;
+using Structor.CLI.Constants;
+using System.IO.Compression;
+
+namespace Structor.CLI.Helpers;
+
+public class FilesUtils
+{
+    public static void CreateDirectoryIfNotExists(string downloadDirectory)
+    {
+        if (!Directory.Exists(downloadDirectory))
+        {
+            Directory.CreateDirectory(downloadDirectory);
+        }
+    }
+    public static async Task<HttpResponseMessage> DownloadFile(string url)
+    {
+        using HttpClient httpClient = new HttpClient();
+        HttpResponseMessage response;
+        response = await httpClient.GetAsync(Consts.PROJECT_GITHUB_ZIP_URL);
+
+        return response;
+    }
+    public static async Task SaveResponseToFile(HttpResponseMessage response, string path)
+    {
+        using var fs = new FileStream(path, FileMode.Create);
+
+        await response.Content.CopyToAsync(fs);
+
+    }
+    public static void UnzipFile(string destinationPath, string zipFilePath, bool deleteAfterUnZip = false)
+    {
+        if (File.Exists(zipFilePath))
+        {
+            ZipFile.ExtractToDirectory(zipFilePath, destinationPath);
+            if (deleteAfterUnZip)
+            {
+                File.Delete(zipFilePath);
+            }
+        }
+    }
+    public static void RenameDirectory(string path, string oldName, string newName)
+    {
+        Directory.Move(Path.Combine(path, oldName), Path.Combine(path, newName));
+    }
+
+    public static void RenameFile(FileInfo file, string newName, string oldName)
+    {
+        var newNamePath = file.Name.Replace(oldName, newName);
+        if (file.Name.Contains(oldName))
+        {
+            file.MoveTo(Path.Combine(file.Directory!.FullName, newNamePath));
+        }
+    }
+    public static void RenameFile(string path, string newName, string oldName)
+    {
+        var file = new FileInfo(path);
+        var newFileName = file.Name.Replace(oldName, newName);
+        if (file.Name.Contains(oldName))
+        {
+            file.MoveTo(Path.Combine(file.Directory!.FullName, newFileName));
+        }
+    }
+}
---
+++ Helpers/GenerationUtils.cs
@@ -0,0 +1,16 @@
+﻿using Structor.CLI.Constants;
+
+namespace Structor.CLI.Helpers;
+
+public static class GenerationUtils
+{
+    public static void RenameSolutionFolder(string path, string newName)
+    {
+        FilesUtils.RenameDirectory(path, Consts.SOLUTION_FOLDER_NAME, newName);
+    }
+    public static void RenameSolutionFile(string path, string newName)
+    {
+        var slnPath = Path.Combine(path, newName, $"{Consts.SOLUTION_NAME}.sln");
+        FilesUtils.RenameFile(slnPath, newName, Consts.SOLUTION_NAME);
+    }
+}
