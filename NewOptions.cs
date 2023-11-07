c45eba420dfef51c89c4341fdcab043b85066397
iMuhammadMustafa <29302054+iMuhammadMustafa@users.noreply.github.com>
2023-11-07 17:20:48 -0600

    Add Project Options

---
+++ NewOptions.cs
@@ -0,0 +1,19 @@
+﻿using CommandLine;
+
+namespace Structor.CLI;
+
+
+[Verb("new", HelpText = "Scaffold new files.")]
+public class NewOptions
+{
+    [Option('p', "project", Required = false, HelpText = "Create a new project.")]
+    public bool Project { get; set; }
+    [Option('f', "feature", Required = false, HelpText = "Create a new feature.")]
+    public bool Feature { get; set; }
+    [Option('d', "domain", Required = false, HelpText = "Create a new domain.")]
+    public bool Domain { get; set; }
+}
+
+
+
+
