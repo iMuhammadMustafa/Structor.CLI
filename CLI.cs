3ad3c2ac342b5c8a1ede22f6846ddcbefb4bf599
iMuhammadMustafa <29302054+iMuhammadMustafa@users.noreply.github.com>
2023-11-07 16:59:09 -0600

    Add CLI Helper Class
    Add PrintWelcome Method

---
+++ CLI.cs
@@ -0,0 +1,18 @@
+﻿using CommandLine.Text;
+using Spectre.Console;
+
+namespace Structor.CLI;
+
+public static class CLI
+{
+    public static void PrintWelcome()
+    {
+        var header = new FigletText("Structor CLI").Color(Color.Green);
+
+        var panel = new Panel(Align.Center(header)).Collapse();
+
+        AnsiConsole.Write(panel);
+        Console.WriteLine(HeadingInfo.Default);
+    }
+}
+
